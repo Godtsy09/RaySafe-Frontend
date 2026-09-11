68961260eadfdd4194d261cd317110840c53064e
Hansel Rodrigo Escobar Pichilla <hescobar-2025439@kinal.edu.gt>
2026-09-11 02:19:26 -0600

    Creacion de la vista: Agregar Agentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { RegisterAgentComponent } from './views/admin/registerAgent';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'register-agent', component: RegisterAgentComponent }
+];
---
+++ src/app/views/admin/registerAgent.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-register-agent',
+    standalone: true,
+    templateUrl: './registerAgent.html',
+    styleUrl: './registerAgent.scss'
+})
+export class RegisterAgentComponent {}
