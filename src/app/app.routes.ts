924333bb3bc7cedd5f69752e75015ceded823ef9
Hansel Rodrigo Escobar Pichilla <hescobar-2025439@kinal.edu.gt>
2026-09-11 21:20:20 -0600

    Creacion de la vista: Login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { RegisterAgentComponent } from './views/admin/registerAgent';
+import { LoginComponent } from './views/auth/login';
 
 export const routes: Routes = [
-  { path: 'register-agent', component: RegisterAgentComponent }
+  { path: 'register-agent', component: RegisterAgentComponent },
+  { path: 'login', component: LoginComponent}
 ];
