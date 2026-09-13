b64cd1a86cee123ab96138ba9cbc8910ffda4021
Hansel Rodrigo Escobar Pichilla <hescobar-2025439@kinal.edu.gt>
2026-09-12 18:02:06 -0600

    Creacion de la vista: start. y pequeñas modificaciones en la vista: login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { RegisterAgentComponent } from './views/admin/registerAgent';
 import { LoginComponent } from './views/auth/login';
+import { StartComponent } from './views/user/start';
 
 export const routes: Routes = [
   { path: 'register-agent', component: RegisterAgentComponent },
-  { path: 'login', component: LoginComponent}
+  { path: 'login', component: LoginComponent},
+  { path: 'start', component: StartComponent}
 ];
---
+++ src/app/views/user/start.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-start',
+    standalone: true,
+    templateUrl: './start.html',
+    styleUrl: './start.scss'
+})
+export class StartComponent{}
