d3b16fb1a26e4e69bfe43d1175a9d9cfe88932d6
Godtsy09 <jjuarez-2025316@kinal.edu.gt>
2026-09-15 18:08:08 -0600

    elaboracion de los siguientes componentes: agent-list, agent-logs, assigned-reports, unassigned-reports

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AgentList } from './views/admin/agent-list/agent-list';
+import { AgentLogs } from './views/admin/agent-logs/agent-logs';
+import { UnassignedReports } from './views/agent/unassigned-reports/unassigned-reports';
+import { AssignedReports } from './views/agent/assigned-reports/assigned-reports';
+
 
 @Component({
-  imports: [RouterOutlet],
   selector: 'app-root',
+  imports: [RouterOutlet, AgentList, AgentLogs,UnassignedReports,AssignedReports], // Agregamos AgentListComponent aquí
   styleUrl: './app.css',
   templateUrl: './app.html',
 })
