60a4f6be9f0332403082f5b4e6257df98c917686
Godtsy09 <jjuarez-2025316@kinal.edu.gt>
2026-09-16 22:26:46 -0600

    modificacion de los estilos css

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,11 +4,17 @@ import { AgentList } from './views/admin/agent-list/agent-list';
 import { AgentLogs } from './views/admin/agent-logs/agent-logs';
 import { UnassignedReports } from './views/agent/unassigned-reports/unassigned-reports';
 import { AssignedReports } from './views/agent/assigned-reports/assigned-reports';
+import { AdjuntarEvidencia } from './views/user/adjuntar-evidencia/adjuntar-evidencia';
+import { ConsultarDenuncia } from './views/user/consultar-denuncia/consultar-denuncia';
+import { EstadisticasDeAbuso } from './views/user/estadisticas-abuso/estadisticas-abuso';
+import { RealizarDenunciaComponent } from './views/user/realizar-denuncia/realizar-denuncia';
+import { RecursosDeAyuda } from './views/user/recursos-de-ayuda/recursos-de-ayuda';
+
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AgentList, AgentLogs,UnassignedReports,AssignedReports], // Agregamos AgentListComponent aquí
+  imports: [RouterOutlet, AgentList, AgentLogs,UnassignedReports,AssignedReports,AdjuntarEvidencia,ConsultarDenuncia, EstadisticasDeAbuso,RealizarDenunciaComponent,RecursosDeAyuda], // Agregamos AgentListComponent aquí
   styleUrl: './app.css',
   templateUrl: './app.html',
 })
